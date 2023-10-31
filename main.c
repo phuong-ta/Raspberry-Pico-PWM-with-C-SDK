2876fe5f81813d0819ad7136343bae0448f81144
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2023-10-31 12:45:40 +0200

    LED pwm added

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
 
+#define PWM_MAX_VALUE 255
 typedef struct IO_PIN {
 int pin_number;
 bool input;
@@ -35,6 +36,8 @@ bool isClicked(PinData  *pin){
     return pressed;
 }
 
+
+
 int main() {
 
     PinData sw0 = { 9, 1};
@@ -45,6 +48,8 @@ int main() {
 
     uint pwm_level = 0;
     bool isOn =0;
+
+
     // Initialize Pins
     InitialPin(&sw0);
     InitialPin(&sw1);
@@ -53,53 +58,62 @@ int main() {
     // Initialize chosen serial port
     stdio_init_all();
 
+    gpio_set_function(led.pin_number, GPIO_FUNC_PWM);
+ 
+    // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
+    uint slice_num = pwm_gpio_to_slice_num(led.pin_number);
+    pwm_set_wrap(slice_num, PWM_MAX_VALUE);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
+    // Set the PWM running
+    pwm_set_enabled(slice_num, true);
     // Loop forever
     while (true) {
-
+        // use flag to check is sw1 pressed (on click) --> then change state of LED
         if (isClicked(&sw1) && sw1_flag==0)
         {
             sw1_flag=1;
             isOn = !isOn;
-            printf("Toggle value %d\n", isOn); // gpio_put(led_pin, false); set led off
-            if(isOn==1)
-            {
-                
-                if (pwm_level==0)
-                {
-                    pwm_level=64;
-                }
-                
-                // press sw2 ==> pwm level --1
-                if (isClicked(&sw2))
-                {
-                    pwm_level-=1;
-                    if (pwm_level<=0)
-                    {
-                        pwm_level=0;
-                    }
-                    printf("PWM Level: %u\r\n", pwm_level);
-                }
-                // press sw0 ==> pwm level ++1
-                if (isClicked(&sw0))
-                {
-                    pwm_level+=1;
-                    if (pwm_level>=125)
-                    {
-                        pwm_level=125;
-                    }
-                    printf("PWM Level: %u\r\n", pwm_level);
-                }
-            }
-            else
-            {
-                gpio_put(led.pin_number, false);
-            }
-            
+            printf("Toggle value %d\n", isOn); // gpio_put(led_pin, false); set led off          
 
         }else if(!isClicked(&sw1)){
             sw1_flag=0;
         }
 
+        // turn on led
+        if(isOn==1)
+        {
+            if (pwm_level==0)
+            {
+                pwm_level=(PWM_MAX_VALUE+1)/2;
+            }
+            pwm_set_chan_level(slice_num, PWM_CHAN_A, pwm_level);  
+            // press sw2 ==> pwm level --1
+            if (isClicked(&sw2))
+            {
+                pwm_level-=1;
+                if (pwm_level<=0)
+                {
+                    pwm_level=0;
+                }
+                printf("PWM Level: %u\r\n", pwm_level);
+            }
+                // press sw0 ==> pwm level ++1
+            if (isClicked(&sw0))
+            {
+                pwm_level+=1;
+                if (pwm_level>=125)
+                {
+                    pwm_level=125;
+                }
+                printf("PWM Level: %u\r\n", pwm_level);
+            }
+        }
+        // turn off led
+        else
+        {
+            //gpio_put(led.pin_number, false);
+            pwm_set_chan_level(slice_num, PWM_CHAN_A, 0); 
+        }
 
         sleep_ms(100);
     }
